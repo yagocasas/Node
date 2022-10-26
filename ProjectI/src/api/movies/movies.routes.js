365f50096f3a97a1ae958c06443b56e4ede112cb
yagocasas <yagocd10@hotmail.com>
2022-10-26 22:21:25 +0200

    ADD movies.routes.js, and GET by: id, title, genre & year

--- ProjectI/src/api/movies/movies.routes.js
+++ ProjectI/src/api/movies/movies.routes.js
@@ -1,15 +1,55 @@
-const express = require('express');
-const Movie = require('./movies.model');
+const express = require("express");
+const Movie = require("./movies.model");
 const router = express.Router();
 
-router.get('/', async(req, res) => {
-    try {
-        const allMovies = await Movie.find();
-        return res.status(200).json(allMovies);
-    } catch (error) {
-        return res.status(500).json(error)
-    }
+router.get("/", async (req, res) => {
+  try {
+    const allMovies = await Movie.find();
+    return res.status(200).json(allMovies);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
+
+router.get("/id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const allMovies = await Movie.findById(id);
+    return res.status(200).json(allMovies);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+router.get("/title/:title", async (req, res) => {
+  try {
+    const title = req.params.title;
+    const allMovies = await Movie.find({ title: title });
+    return res.status(200).json(allMovies);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+router.get("/genre/:genre", async (req, res) => {
+  try {
+    const genre = req.params.genre;
+    const allMovies = await Movie.find({ genre: genre });
+    return res.status(200).json(allMovies);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+router.get("/year/:year", async (req, res) => {
+    try {
+      const year = req.params.year;
+      const allMovies = await Movie.find({ year: {$gte: year }});
+      return res.status(200).json(allMovies);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  });
 
 
 module.exports = router;
--- ProjectI/src/utils/seeds/movies.seed.js
+++ ProjectI/src/utils/seeds/movies.seed.js
@@ -1,64 +1,65 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Movie = require('../../api/movies/movies.model');
+const Movie = require("../../api/movies/movies.model");
 
-require('dotenv').config();
+require("dotenv").config();
 const DB_URL = process.env.DB_URL;
 
 const movies = [
-    {
-      title: "The Matrix",
-      director: "Hermanas Wachowski",
-      year: 1999,
-      genre: "Accion",
-    },
-    {
-      title: "The Matrix Reloaded",
-      director: "Hermanas Wachowski",
-      year: 2003,
-      genre: "Accion",
-    },
-    {
-      title: "Buscando a Nemo",
-      director: "Andrew Stanton",
-      year: 2003,
-      genre: "Animacion",
-    },
-    {
-      title: "Buscando a Dory",
-      director: "Andrew Stanton",
-      year: 2016,
-      genre: "Animacion",
-    },
-    {
-      title: "Interestelar",
-      director: "Christopher Nolan",
-      year: 2014,
-      genre: "Ciencia ficcion",
-    },
-    {
-      title: "50 primeras citas",
-      director: "Peter Segal",
-      year: 2004,
-      genre: "Comedia romantica",
-    },
-  ];
+  {
+    title: "The Matrix",
+    director: "Hermanas Wachowski",
+    year: 1999,
+    genre: "Accion",
+  },
+  {
+    title: "The Matrix Reloaded",
+    director: "Hermanas Wachowski",
+    year: 2003,
+    genre: "Accion",
+  },
+  {
+    title: "Buscando a Nemo",
+    director: "Andrew Stanton",
+    year: 2003,
+    genre: "Animacion",
+  },
+  {
+    title: "Buscando a Dory",
+    director: "Andrew Stanton",
+    year: 2016,
+    genre: "Animacion",
+  },
+  {
+    title: "Interestelar",
+    director: "Christopher Nolan",
+    year: 2014,
+    genre: "Ciencia ficcion",
+  },
+  {
+    title: "50 primeras citas",
+    director: "Peter Segal",
+    year: 2004,
+    genre: "Comedia romantica",
+  },
+];
 
-  mongoose.connect(DB_URL)
+mongoose
+  .connect(DB_URL)
   .then(async () => {
     const allMovies = await Movie.find();
-    if(!allMovies.length){
-        console.log('No se encuentran películas');
+    if (!allMovies.length) {
+      console.log("No se encuentran películas");
     } else {
-        console.log(`Encontradas ${allMovies.Length} películas`);
-        await Movie.collection.drop();
-        console.log(`Se ha eliminado la colección`);
+      console.log(`Encontradas ${allMovies.Length} películas`);
+      await Movie.collection.drop();
+      console.log(`Se ha eliminado la colección`);
     }
   })
   .catch((error) => {
     console.log(`No se han podido eliminar las películas`, error);
   })
-  .then(async() => {
+  .then(async () => {
     await Movie.insertMany(movies);
     console.log(`${movies.length} agregadas a la colección`);
   })
